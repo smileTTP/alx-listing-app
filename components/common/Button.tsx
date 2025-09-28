a4ed42ca339c99c4c4598427f6457e7fc32394a7
SmileTTP <ibtissamlazraf@gmail.com>
2025-09-28 22:49:28 +0100

    components

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,2 +1,8 @@
 import React from "react";
 import { ButtonProps } from "@/interfaces";
+
+export default function Button({label, onClick}: ButtonProps) {
+    return (
+        <button onClick={onClick}>{label}</button>
+    );
+}
--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,2 +1,8 @@
 import React from "react";
 import { CardProps } from "@/interfaces";
+
+export default function Card({title, description, imageUrl}: CardProps) {
+    return(
+        <div></div>
+    );
+}
